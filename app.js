b8f48452241855f104abf3ccc14d82e16b947e15
sadas <tumusiimesadas00@gmail.com>
2026-01-20 16:24:07 +0300

    now it using redirect to login

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 document.getElementById('loginForm').addEventListener('submit', function(e) {
-    // e.preventDefault(); // Allow form submission
+    e.preventDefault(); // Prevent default form submission
     var voucher = document.getElementById('username').value;
-    document.querySelector('input[name="password"]').value = voucher; // Set password to voucher
-    // The form will now submit via POST to the action URL
+    window.location.href = "http://192.168.88.1/login?username=" + encodeURIComponent(voucher) + "&password=" + encodeURIComponent(voucher);
 });
